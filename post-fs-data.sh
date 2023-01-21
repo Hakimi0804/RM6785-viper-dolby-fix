a29b3537809ed9f6c70e1e2e572672dac4bddd81
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2023-01-21 20:11:26 +0800

    Fallback to /data/unencrypted if cache is broken*
    * RM6785 device tree currently does not have f2fs entry
      in its fstab for cache, thus people who format cache
      as f2fs will have the module not working as intended,
      failing silently. Why they do this in the first place?
      some ROM maintainer added cache entry, leading them to
      think it will work on every ROM.

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,5 +1,9 @@
 #!/system/bin/sh
 
+if ! touch /cache/test; then
+    mount -o bind /data/unencrypted /cache
+fi
+
 rm -f /cache/audio_effects.xml
 touch /cache/audio_effects.xml
 chcon u:object_r:vendor_configs_file:s0 /cache/audio_effects.xml
